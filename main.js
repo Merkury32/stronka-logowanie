84fc549854842aca5a623a244803176456787085
Merkury32 <pjozefiak32@gmail.com>
2020-02-20 19:20:39 +0100

    stworzyłem kod do zamiany napisu

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-const myForm = document.getElementById('form-login')
-
-myForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const formData =  new FormData (this);
-
-    fetch('https://www.example.com',{
-        method: 'get',
-    }).then(function(response){
-        return response.text();
-    }).then(function(text){
-        console.log(text);
-    }).catch(function(error){
-        console.error(error);
-    })
-});
+var change = new Vue({
+    el: '#change',
+    data: {
+      message: 'Zaloguj się'
+    },
+    methods: {
+      change: function () {
+        this.message = 'Zalogowany!'
+      }
+    }
+  })
